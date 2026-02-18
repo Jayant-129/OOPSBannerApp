c4bcd5fc673ddf11e67fc9d85ae7a2c811eaba31
Jayant-129 <agrawaljayant86@gmail.com>
2026-02-18 14:23:57 +0530

    Coded UC2 to Print OOPS on Console using *

---
+++ OOPSBannerApp.java
@@ -0,0 +1,20 @@
+/*
+
+OOPSBannerApp UC2 - OOPS Banner Display Application
+
+This class demonstrate a simple Java application that display the 
+Object Oriented Programming System OOPS acroym to the console
+
+@author Developer
+@version 2.0
+*/
+
+public class OOPSBannerApp{
+	public static void main(String args[]){
+		System.out.println("******" + " " + "******" + " " + "******" + " " + "******");   
+		System.out.println("*    *" + " " + "*    *" + " " + "*    *" + " " + "*     ");
+		System.out.println("*    *" + " " + "*    *" + " " + "******" + " " + "******");
+		System.out.println("*    *" + " " + "*    *" + " " + "*     " + " " + "     *");
+		System.out.println("******" + " " + "******" + " " + "*     " + " " + "****** ");
+	}
+}
